8f12f7eebdf140a83ec99f71d9f0a03fd9587e72
Glitch (name-badges-list) <none>
2020-07-12 00:54:28 +0000

    🐗🏟 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,40 @@
+var employees = ["Virginia", "Jason", "Christine", "Melissa", "Nancy", "Kathleen", "Frank", "Jacob", "Elizabeth", "Kathleen", "Brian", "Cynthia", "Dennis", "Laura", "Patrick", "Kathleen", "Ryan", "Anthony", "Brenda", "Paul", "William", "Nancy", "Catherine", "Dennis", "Rachel", "Michael", "Charles", "Ryan", "Timothy", "Jessica", "Gary", "Stephanie", "Catherine", "Edward", "Eric", "James", "Andrew", "Shirley", "Ruth", "Thomas", "Charles", "Sharon", "Cynthia", "Steven", "Christine", "Jack", "Helen", "Margaret", "Samantha", "Betty", "Carol", "Patricia", "Janet", "Carolyn", "Anna", "Brenda", "George", "Linda", "Scott", "Katherine", "Eric", "Jessica", "Nicole", "Jennifer", "Melissa", "Jennifer", "Debra", "Patricia", "Gregory", "Laura", "Samantha", "Dennis", "Ronald", "Jack", "Matthew", "George", "Kathleen", "Thomas", "Betty", "David", "Stephanie", "Joseph", "Emma", "Brenda", "Samantha", "Eric", "Kenneth", "Daniel", "Amanda", "Sharon", "Janet", "Thomas", "Timothy", "Anna", "James", "Amanda", "Andrew", "Helen", "Michelle", "William", "Samantha", "Stephen", "Janet", "Nicholas", "Brandon", "John", "Carolyn", "Elizabeth", "Stephen", "Betty", "Dorothy", "Nicholas", "Robert", "Michelle", "Rebecca", "Karen", "Emma", "William", "Deborah", "Patrick", "James", "Kenneth", "Margaret", "Jack", "Jessica", "Jennifer", "Thomas", "Ashley", "Virginia", "Pamela", "John", "Amanda", "Helen", "Daniel", "Jessica", "Robert", "Barbara", "Eric", "Jerry", "Angela", "Jessica", "Helen", "Dorothy", "Carol", "Andrew", "Anthony", "Patrick", "Joshua", "Susan", "Laura", "Virginia", "Brenda", "Daniel", "Susan", "Ryan", "Linda", "Laura", "Lisa", "Carol", "Laura", "Mark", "Joshua", "Joshua", "Amy", "Anna", "Amanda", "Anthony", "Mary", "Justin", "Charles", "Patrick", "Barbara", "Melissa", "Angela", "Steven", "Melissa", "Alexander", "Joseph", "Kevin", "Cynthia", "Raymond", "Carolyn", "Steven", "Scott", "Joshua", "Justin", "Sharon", "Jacob", "Mark", "Alexander", "Barbara", "Helen", "Dorothy", "Christine", "Patrick", "Kimberly", "Nicole", "Daniel", "Virginia", "Nicholas", "Richard", "Lisa", "Patrick", "Nicole", "George", "Scott", "George", "Samantha", "Patrick", "Nicholas", "Dennis", "Margaret", "Linda", "Margaret", "Emma", "Lisa", "Karen", "Nicholas", "Matthew", "Carol"]
+
+
+// In the lines above, there is a list of first names of all the people who work for a new app called Badges.
+// Their names are stored in a list called "employees".
+// For their holiday party, Hamadou (the CEO) wants to make personalized mugs for each one with their names on it, but the trouble is that a lot of workers have the same first names.
+// They'll save a lot of money if they can know how many of each name to make. Jose is asking you, his newest engineer, to order the mugs.
+
+// 1. How many total mugs will you need to order? In other words, how many people work for the company?
+
+
+
+
+// 2. How many DIFFERENT names will you need to print? In other words, how many unique, individual names are there in the company?
+
+
+
+
+// 3. Almost forgot! Hamadou also needs you to print name tags for everyone for the holiday party.
+//    For every name in the employees list, print out a string that says "Hello! My name is ______"
+
+
+
+
+
+// 4. Let's practice counting just one name. Find out what the first name in the list is.
+//     Then, iterate over the list and see how many people in the list share that same name.
+
+
+
+
+
+// 5. CHALLENGE: Alright, let's finish the job: create the list of how many mugs per name. HINT: You may need to review documentation for this.
+
+
+
+
+
+
+// 6. CHALLENGE PART 2: Before you send it off to the printers, you should check your work. Make sure your final list has the same number of names as you found in #2, and orders the same number of mugs you found in number 1
